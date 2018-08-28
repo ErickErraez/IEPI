2330ea3aeb448627a4d4bcb6aa5c27b94c0f6645
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-08-28 16:59:45 -0500

    Movies

--- AmazonViewer/src/edu/yavirac/amazonviewer/Main.java
+++ AmazonViewer/src/edu/yavirac/amazonviewer/Main.java
@@ -1,46 +1,51 @@
 package edu.yavirac.amazonviewer;
 
+import edu.yavirac.amazonviewer.model.*;
 import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    boolean exit = true;
-    Scanner sc = new Scanner(System.in);
-    
-    do {
-      System.out.println("\nBIENVENIDOS AMAZON VIEWER");
-      System.out.println("1. Movies");
-      System.out.println("2. Series");
-      System.out.println("3. Books");
-      System.out.println("4. Magazines");
-      System.out.println("0. Exit");
-      System.out.print("Selecciona el numero de la opción deseada: ");
+	public static void main(String[] args) {
 
-      int entrada = sc.nextInt(); // leer la respuesta del usuario
-      
-      switch (entrada) {
-      case 1:
-        System.out.println("Entraste a Movies!");
-        break;
-      case 2:
-        System.out.println("Entraste a Series!");
-        break;
-      case 3:
-        System.out.println("Entraste a Books!");
-        break;
-      case 4:
-        System.out.println("Entraste a Magazines!");
-        break;
-      case 0:
-        System.out.println("Saliste del sistema!");
-        exit = false;
-        break;
-      default:
-        System.out.println("Opción no disponible. Intenta de nuevo...");
-        break;
-      }
-    } while (exit);
-    sc.close();
-  }
+		Movie movie = new Movie();
+		movie.title = "Coco";
+		movie.duration = 120;
+		boolean exit = true;
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.println("\nBIENVENIDOS AMAZON VIEWER");
+			System.out.println("1. Movies");
+			System.out.println("2. Series");
+			System.out.println("3. Books");
+			System.out.println("4. Magazines");
+			System.out.println("0. Exit");
+			System.out.print("Selecciona el numero de la opción deseada: ");
+
+			int entrada = sc.nextInt(); // leer la respuesta del usuario
+
+			switch (entrada) {
+			case 1:
+				System.out.println("Entraste a Movies!");
+				break;
+			case 2:
+				System.out.println("Entraste a Series!");
+				break;
+			case 3:
+				System.out.println("Entraste a Books!");
+				break;
+			case 4:
+				System.out.println("Entraste a Magazines!");
+				break;
+			case 0:
+				System.out.println("Saliste del sistema!");
+				exit = false;
+				break;
+			default:
+				System.out.println("Opción no disponible. Intenta de nuevo...");
+				break;
+			}
+		} while (exit);
+		sc.close();
+	}
 }
