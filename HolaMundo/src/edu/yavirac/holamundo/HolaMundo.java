368d812481edbfe8441b47d32bb42669be683edf
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-08-28 14:02:03 -0500

    Curso Java

---
+++ HolaMundo/src/edu/yavirac/holamundo/HolaMundo.java
@@ -0,0 +1,61 @@
+package edu.yavirac.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("Hola Mundo IDE :)");
+				
+				//Enteros
+				byte edad = 127;
+				short year = -32768;
+				int id_user = 1001; 
+				long id_twitter = 12345678923456789L;
+				
+				
+				//Punto Flotante
+				float diametro = 34.25F;
+				double precio = 12345.2345678923456723456234;
+				
+				//Texto
+				char genero = 'M';
+				
+				//Logico
+				boolean isVisible = true;
+				boolean funciona = false;
+				
+				int variable = 2;
+				int _variable = 3;
+				int $variable = 4;
+				int variable1 = 5;
+				
+				
+				//Constantes
+				int VALOR = 0;
+				int VALOR_MAXIMO = 1;
+				
+				
+				
+				//Lower Camel Case
+				int minValor = 4;
+				
+				byte b = 6;
+				short s = b;
+				
+				b = (byte) s;
+				
+				int i = 1;
+				double d = 2.8;
+				i= (int) d;
+				
+				int codigo = 97;
+				char codigoASCII = (char) codigo;
+				
+				short numero = 300;
+				byte numeroByte = (byte) numero;
+				
+				System.out.println(numeroByte);
+			
+	}
+
+}
