40ee9ee05fb837c09447fb0cbc7d216bf8fe7cea
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-08-30 17:12:40 -0500

    Connecion a la Base y Creacion Constantes

---
+++ AmazonViewer/src/edu/yavirac/amazonviewer/db/IDBConnection.java
@@ -0,0 +1,25 @@
+package edu.yavirac.amazonviewer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import static edu.yavirac.amazonviewer.db.DataBase.*;
+
+public interface IDBConnection {
+
+	default Connection connetToDB() {
+		Connection connection = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL + DB, USER, PASSWORD);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			return connection;
+		}
+
+	}
+
+}
